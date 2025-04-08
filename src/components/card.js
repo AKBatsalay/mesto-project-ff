a08881161b4f0d8b93c9ad691304c7b5a9083730
Nastya Batsalay <batsalayak@gmail.com>
2025-04-08 16:10:00 +0300

    Доработка ошибок

--- src/components/card.js
+++ src/components/card.js
@@ -1,38 +1,38 @@
-
-
-
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
-export const createCard = ({name, link}, deleteCard, handleLike, handleImageClick) => {   
-    const cardsElement  = cardTemplate.querySelector('.card').cloneNode(true);
-    const likeButton = cardsElement.querySelector('.card__like-button');
-    const cardImage = cardsElement.querySelector('.card__image');  
-    const deleteButton = cardsElement.querySelector('.card__delete-button');
+export const createCard = (
+  { name, link },
+  deleteCard,
+  handleLike,
+  handleImageClick
+) => {
+  const cardsElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const likeButton = cardsElement.querySelector(".card__like-button");
+  const cardImage = cardsElement.querySelector(".card__image");
+  const deleteButton = cardsElement.querySelector(".card__delete-button");
 
-    cardsElement.querySelector('.card__title').textContent = name;
-    cardsElement.querySelector('.card__image').alt = name;
-    cardsElement.querySelector('.card__image').src = link;
-   
-    deleteButton.addEventListener('click',() => deleteCard(cardsElement));
+  cardsElement.querySelector(".card__title").textContent = name;
+  cardsElement.querySelector(".card__image").alt = name;
+  cardsElement.querySelector(".card__image").src = link;
 
-    cardImage.addEventListener('click', () => handleImageClick(name, link));
+  deleteButton.addEventListener("click", () => deleteCard(cardsElement));
 
-    likeButton.addEventListener('click', handleLike);
-   
-    return cardsElement;
-}
+  cardImage.addEventListener("click", () => handleImageClick(name, link));
 
+  likeButton.addEventListener("click", handleLike);
+
+  return cardsElement;
+};
 
 export function handleLike(evt) {
-  if(evt.target.classList.contains('card__like-button'))
-   evt.target.classList.toggle('card__like-button_is-active');
-};
+  if (evt.target.classList.contains("card__like-button"))
+    evt.target.classList.toggle("card__like-button_is-active");
+}
 
 // @todo: Функция удаления карточки
 export function deleteCard(cardsElement) {
-    cardsElement.remove(); 
+  cardsElement.remove();
 }
-
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,27 +1,16 @@
-const popups = document.querySelectorAll('.popup'); //фон попап окна
-
 export const openPopup = (popup) => {
-  popup.classList.add('popup_is-opened');
-  document.addEventListener('keyup', handleEscKeyUp);
-}
+  popup.classList.add("popup_is-opened");
+  document.addEventListener("keyup", handleEscKeyUp);
+};
 
 export const closePopup = (popup) => {
-  popup.classList.remove('popup_is-opened');
-  document.removeEventListener('keyup', handleEscKeyUp);
-}
+  popup.classList.remove("popup_is-opened");
+  document.removeEventListener("keyup", handleEscKeyUp);
+};
 
 export function handleEscKeyUp(evt) {
-  if (evt.key === 'Escape') {
-    const popupOpened = document.querySelector('.popup_is-opened');
+  if (evt.key === "Escape") {
+    const popupOpened = document.querySelector(".popup_is-opened");
     closePopup(popupOpened);
-    }
-  };
-
-  popups.forEach(popup => {
-    popup.addEventListener('click', (event) => {
-       if (event.target === popup) {
-       closePopup(popup); 
-     }
-   });
-  });    
- 
+  }
+}
